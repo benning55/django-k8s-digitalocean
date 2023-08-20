5c3b809bf139cfd075090ad68c3830387a8933ae
sedthawuth.m <sedthawuth.m@lmwn.com>
2023-08-20 23:10:31 +0700

    changes

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -152,3 +153,8 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = BASE_DIR / "staticfiles-cdn"
+
+STATicFILES_DIRS = [BASE_DIR / "staticfiles"]
+from .cdn.conf import *  # noqa
